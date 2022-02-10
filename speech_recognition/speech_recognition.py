speech_recognition minor changes; wrong index fix

speech_recognition/speech_recognition.py
@@ -9,7 +9,8 @@ from comet_ml import Experiment
 from train import train
 # from evaluate import evaluate
 
-def speech_recognition(learning_rate=5e-4, epochs=10):
+
+def speech_recognition(srt_directory, wav_directory, learning_rate=5e-4, epochs=10):
 
     hparams = {
         'n_cnn_layers': 3,
@@ -27,7 +28,9 @@ def speech_recognition(learning_rate=5e-4, epochs=10):
     torch.manual_seed(7)
     device = torch.device('cpu')
 
-    train_dataset = torchaudio.datasets.LIBRISPEECH('./train', url='train-clean-100', download=True)
+    train_dataset = torchaudio.datasets.LIBRISPEECH('./train',
+                                                    url='train-clean-100',
+                                                    download=True)
 
     train_dataset = DataLoader(dataset=train_dataset,
                                batch_size=hparams['batch_size'],
@@ -56,4 +59,8 @@ def speech_recognition(learning_rate=5e-4, epochs=10):
     for epoch in range(1, epochs + 1):
         train(model, device, train_dataset, criterion, opt, schd, epoch, experiment)
 
-speech_recognition()
+    # evaluate(srt_directory, wav_directory)
+
+
+
+speech_recognition('', '')

speech_recognition/speech_recognition_module.py
@@ -103,7 +103,7 @@ class SpeechRecognitionModel(nn.Module):
         x = self.rescnn_layers(x)
         sizes = x.size()
         x = x.view(sizes[0], sizes[1] * sizes[2], sizes[3])  # (batch, feature, time)
-        x = DataManipulation.transpose_position(x, 2)  # (batch, time, feature)
+        x = DataManipulation.transpose_position(x, 1)  # (batch, time, feature)
         x = self.fully_connected(x)
         x = self.birnn_layers(x)
         x = self.classifier(x)