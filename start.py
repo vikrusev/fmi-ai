43e2b3702e38388fc3fbba0f376b80a8813436a8
Viktor Rusev <vikrusev257@gmail.com>
2022-02-10 21:55:49 +0200

    resolve merge ariolandi -> viko

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ from speech_recognition.audio_preparation import AudioAnalisys, construct_subtit
 from translation.translation import translate
 from models_enum import Models
 
-filename_video = 'phone-message-jacko-its-pete.mp4'
+filename_video = 'speech.mp4'
 speech_recognition_directory = 'speech_recognition/'
 
 wav_path = 'sound_files/'
@@ -13,8 +13,8 @@ wav_path_full = speech_recognition_directory + wav_path
 aa = AudioAnalisys(filename_video, wav_path_full, str_path)
 aa.chunk_audio()
 
-str_path = 'dummy_str_files/'
-construct_subtitres(filename_video, str_path)
+# str_path = 'dummy_str_files/'
+# construct_subtitres(filename_video, str_path)
 
 # define languages here
 # the program expects to find vocabulary files in /translations/vocabulary/vocab_{lang}
