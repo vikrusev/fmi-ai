d97a73fe7fa19c27d0c27128b0624c7348dc7491
Viktor Rusev <vikrusev257@gmail.com>
2022-02-10 22:07:44 +0200

    fix srt typo

--- start.py
+++ start.py
@@ -7,14 +7,14 @@ filename_video = 'speech.mp4'
 speech_recognition_directory = 'speech_recognition/'
 
 wav_path = 'sound_files/'
-str_path = 'str_files/'
+str_path = 'srt_files/'
 wav_path_full = speech_recognition_directory + wav_path
 str_path_full = speech_recognition_directory + str_path
 
 aa = AudioAnalisys(filename_video, wav_path_full, str_path_full)
 aa.chunk_audio()
 
-# str_path = 'dummy_str_files/'
+# str_path = 'dummy_srt_files/'
 # construct_subtitres(filename_video, str_path)
 
 # define languages here
